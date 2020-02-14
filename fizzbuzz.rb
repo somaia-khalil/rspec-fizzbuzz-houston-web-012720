e6eb655e90516c4e36c35a1bc2c2341edc1269da
Somaia Khalil <sumaya_khair@live.com>
2020-02-14 03:41:10 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,11 +3,11 @@
 
 def fizzbuzz (int)
   if int % 3 == 0 
-    puts "Fizz"
+    return "Fizz"
   elsif int% 5 == 0 
-  puts "Buzz"
+  return "Buzz"
 elsif int % 3 == 0 && int % 5 == 0 
-puts "FizzBuzz"
+return "FizzBuzz"
 else 
   nil
 end
